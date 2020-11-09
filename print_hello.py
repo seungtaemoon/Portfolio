40002f399b64e7d340827e846a412dd886bb4904
conanmoon <72179917+conanmoon@users.noreply.github.com>
2020-11-09 09:40:43 +0900

    First commit

---
+++ print_hello.py
@@ -0,0 +1,9 @@
+## print_hello.py
+ 
+#print('hello')
+ 
+def bye():
+    print('bye')
+
+if __name__ == "__main__":
+    print('hello')
---
+++ print_numbers.py
@@ -0,0 +1,8 @@
+## print_numbers.py
+ 
+import print_hello
+ 
+print('numbers')
+
+print_hello.bye()
+
